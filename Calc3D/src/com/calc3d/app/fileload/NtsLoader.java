09d3c6033a1fc2fa943c18682f555fe7e0e487f3
unknown <lucasasecas@gmail.com>
2014-10-12 08:01:08 -0300

    git closes #16: procrustes robust bug fixed

---
+++ Calc3D/src/com/calc3d/app/fileload/NtsLoader.java
@@ -0,0 +1,15 @@
+package com.calc3d.app.fileload;
+
+public class NtsLoader extends FileLoader{
+
+	public static final int RECTANGULAR_MATRIX = 0;
+	public static final int SYMMETRIC_DISSIMILARITY_MATRIX = 1;
+	
+	
+	@Override
+	public Object load(String path) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
